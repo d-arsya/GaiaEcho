70b8472ca57df02c4686ecb376227da5d60fb66c
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-07-18 22:42:00 +0700

    Seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Bookmark;
+use App\Models\Comment;
+use App\Models\Followee;
+use App\Models\Like;
 use App\Models\User;
+use App\Models\Post;
+use App\Models\Report;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +21,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory(10)->create();
+        Report::factory(30)->create();
+        Article::factory(20)->create();
+        Post::factory(50)->create();
+        Followee::factory(60)->create();
+        Bookmark::factory(50)->create();
+        Comment::factory(50)->create();
+        Like::factory(50)->create();
+        User::create([
+            "name"=>"Admin",
+            "avatar"=>"Admin",
+            "username"=>"Admin",
+            "email"=>"kamaluddin.arsyad17@gmail.com",
+            "password"=>Hash::make('admin'),
+            "role"=>"admin"
         ]);
     }
 }
